591a5db097feb32cc9517a826fb77eea817a69c4
Ahmed R <ahmedrizwan239@gmail.com>
2023-10-02 02:08:35 +0500

    Features

--- lib/screens/todo.dart
+++ lib/screens/todo.dart
@@ -8,8 +8,14 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoListState extends State<TodoList> {
-  List<String> taskList = ['Eat', 'Sleep', 'Repeat'];
+  List<TaskItem> taskList = [
+    TaskItem(title: 'Eat', isCompleted: false),
+    TaskItem(title: 'Sleep', isCompleted: false),
+    TaskItem(title: 'Repeat', isCompleted: false),
+  ];
+
   TextEditingController taskController = TextEditingController();
+  int editIndex = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +32,38 @@ class _TodoListState extends State<TodoList> {
             child: TextField(
               controller: taskController,
               decoration: InputDecoration(
-                hintText: 'Add a task',
+                hintText: editIndex == -1 ? 'Add a task' : 'Edit task',
                 suffixIcon: IconButton(
-                  icon: Icon(Icons.add),
+                  icon: Icon(editIndex == -1 ? Icons.add : Icons.check),
                   onPressed: () {
                     // Add the task to the taskList
                     setState(() {
-                      taskList.add(taskController.text);
-                      taskController.clear();
+                      if(taskController.text.isNotEmpty)
+                      {
+                        if (editIndex == -1) {
+                          taskList.add(TaskItem(title: taskController.text, isCompleted: false));
+                        } 
+                        else {
+                          taskList[editIndex].title = taskController.text;
+                          editIndex = -1;
+                        }
+                        taskController.clear();
+                      }
+                      else
+                      {
+                        // Display a styled Snackbar when the task text is empty
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(
+                            content: Text("Please enter a task!"),
+                            backgroundColor: Colors.red, 
+                            elevation: 6, 
+                            behavior: SnackBarBehavior.floating, 
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        );
+                      }
                     });
                   },
                 ),
@@ -45,9 +75,10 @@ class _TodoListState extends State<TodoList> {
             child: ListView.builder(
               itemCount: taskList.length,
               itemBuilder: (context, index) {
-                var item = taskList[index];
+                var task = taskList[index];
                 return Card(
                   elevation: 10,
+                  shadowColor: Colors.purple[400],
                   child: ListTile(
                     contentPadding: EdgeInsets.symmetric(vertical: 8),
                     leading: CircleAvatar(
@@ -55,40 +86,64 @@ class _TodoListState extends State<TodoList> {
                       child: Text((index + 1).toString()),
                     ),
                     title: Text(
-                      item,
-                      style: const TextStyle(
+                      task.title,
+                      style: TextStyle(
                         fontWeight: FontWeight.bold,
+                        decoration: task.isCompleted ? TextDecoration.lineThrough : TextDecoration.none,
                       ),
                     ),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        var alert = AlertDialog(
-                          title: Text("Confirm delete"),
-                          content: Text("Are you sure you want to delete?"),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text("No", style: TextStyle(color: Colors.red)),
-                            ),
-                            TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  taskList.removeAt(index);
-                                });
-                                Navigator.of(context).pop();
-                              },
-                              child: Text("Yes", style: TextStyle(color: Colors.red)),
-                            ),
-                          ],
-                        );
-                        showDialog(
-                          context: context,
-                          builder: (context) => alert,
-                        );
-                      },
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Checkbox(
+                          value: task.isCompleted,
+                          activeColor: Colors.purple,
+                          onChanged: (newValue) {
+                            setState(() {
+                              task.isCompleted = !task.isCompleted;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.edit), color: Colors.purple,
+                          onPressed: () {
+                            setState(() {
+                              editIndex = index;
+                              taskController.text = task.title;
+                            });
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete), color: Colors.purple,
+                          onPressed: () {
+                            var alert = AlertDialog(
+                              title: Text("Confirm delete"),
+                              content: Text("Are you sure you want to delete?"),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text("No", style: TextStyle(color: Colors.red)),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      taskList.removeAt(index);
+                                    });
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text("Yes", style: TextStyle(color: Colors.red)),
+                                ),
+                              ],
+                            );
+                            showDialog(
+                              context: context,
+                              builder: (context) => alert,
+                            );
+                          },
+                        ),
+                      ],
                     ),
                   ),
                 );
@@ -99,4 +154,11 @@ class _TodoListState extends State<TodoList> {
       ),
     );
   }
+}
+
+class TaskItem {
+  String title;
+  bool isCompleted;
+
+  TaskItem({required this.title, required this.isCompleted});
 }
